5cb528f00055d6eab478c3d4c9c08a74f941989a
Oleksandr <o.grument@gmail.com>
2017-01-26 12:40:55 +0200

    123

--- src/main/java/com/SoftwareFactory/controller/UserCabinetController.java
+++ src/main/java/com/SoftwareFactory/controller/UserCabinetController.java
@@ -19,7 +19,7 @@ import java.util.Locale;
 public class UserCabinetController {
 
 
-/*shdfjshd5fgtsdgjk*/
+/*shdfjshd5fgt4546sdgjk*/
 
     @RequestMapping(value = "/cabinet", method = RequestMethod.GET)
     public ModelAndView cabinet(Locale locale , HttpSession httpSession) {
