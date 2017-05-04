352f42d45397fc93194ad06ece447aa6c9c9fa21
EvgeniyMurenko <mr_jeka@bk.ru>
2017-05-04 18:15:31 +0300

    app config

--- src/main/java/com/SoftwareFactoryAdmin/constant/GlobalEnum.java
+++ src/main/java/com/SoftwareFactoryAdmin/constant/GlobalEnum.java
@@ -2,7 +2,7 @@ package com.SoftwareFactoryAdmin.constant;
 
 
 public enum  GlobalEnum {
-/*    webRoot ("http://www.sofac.kr");*/
+/*    webRoot ("http://www.a.sofac.kr");*/
    /* webRoot ("http://localhost:8080");*/
     webRoot("http://52.211.242.225:8080");
 
