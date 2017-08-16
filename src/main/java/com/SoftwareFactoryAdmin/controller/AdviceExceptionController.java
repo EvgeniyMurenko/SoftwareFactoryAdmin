43e7ffc67ee896aa580a5012ea815f9b2e4cf252
Oleksandr <o.grument@gmail.com>
2017-08-16 15:30:11 +0300

    ex

--- src/main/java/com/SoftwareFactoryAdmin/controller/AdviceExceptionController.java
+++ src/main/java/com/SoftwareFactoryAdmin/controller/AdviceExceptionController.java
@@ -1,24 +1,47 @@
 /*
 package com.SoftwareFactoryAdmin.controller;
 
+import com.SoftwareFactoryAdmin.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 
 @ControllerAdvice
 @RequestMapping("/error")
 public class AdviceExceptionController {
 
+
+    @Autowired
+    MailService mailService;
+
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleAll(Exception ex){
+    public ModelAndView handleAll(Exception ex) {
+
+
+
+        if (!FileNotFoundException.class.isInstance(ex)) {
+
+            StringWriter sw = new StringWriter();
+            ex.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            mailService.sendBugExceptionToEmail(exceptionAsString);
+
+        }
+
+
         ModelAndView exceptionView;
-        if (NoHandlerFoundException.class.isInstance(ex)){
-            exceptionView =new  ModelAndView("redirect:/error/404");
+        if (NoHandlerFoundException.class.isInstance(ex)) {
+            exceptionView = new ModelAndView("redirect:/error/404");
         } else {
             exceptionView = new ModelAndView("404");
-            exceptionView.addObject("number" , ex);
-            exceptionView.addObject("error" ,ex.getMessage());
+            exceptionView.addObject("number", ex);
+            exceptionView.addObject("error", ex.getMessage());
         }
         return exceptionView;
     }
@@ -33,5 +56,4 @@ public class AdviceExceptionController {
         return new ModelAndView("404");
     }
 }
-
 */
